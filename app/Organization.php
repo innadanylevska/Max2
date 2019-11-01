8ce48e7ca810778981e87ef3dae69a793b11ae79
DInnaD <DInnaD@users.noreply.github.com>
2019-11-01 14:23:42 +0200

    Update Organization.php

--- app/Organization.php
+++ app/Organization.php
@@ -67,4 +67,41 @@ class Organization extends Model
         $softDelete = \App\Organization::onlyTrashed()->count();
         return $organization = collect(['active' =>  $active, 'softDelete' => $softDelete, 'all' => $all]);
     }
+
+    public static function getShowList(Request $request, Organization $organization)
+    {       
+        $vacancies = $request->input('vacancies', null);
+        $workers = $request->input('workers', null);
+        $only_active = $request->input('only_active', null);
+        $workers_amount = $request->get('workers_amount', 0);  
+
+        $builder=Vacancy::query();
+        $builder->select('vacancies.*', 'w.workers_booked', DB::raw("IF(
+            vacancies.workers_amount > w.workers_booked, 
+            'active',
+            'closed'
+            ) AS status"));
+        $subQueryWorkers = DB::query()
+            ->select('vacancy_id', DB::raw('COUNT(vacancy_id) AS workers_booked'))
+            ->from('user_vacancy')
+            ->groupBy('vacancy_id');            
+        $builder->leftJoinSub($subQueryWorkers,'w', function ($query) use ($organization){
+            $query->on('w.vacancy_id', 'vacancies.id')->where('vacancies.organization_id', '=', $organization->id);
+            //->where('w.vacancy_id', 'vacancies.organization_id');
+        });
+        if ($vacancies == '1' && $workers == '1') {
+            $builder->whereRaw('vacancies.workers_amount <= w.workers_booked');
+        }elseif($vacancies == '2'&& $workers == '1'){
+            $builder->whereRaw('vacancies.workers_amount > w.workers_booked');            
+        }elseif($vacancies == '3'&& $workers == '1'){
+            $builder->whereRaw('vacancies.workers_amount <= w.workers_booked' and 'vacancies.workers_amount > w.workers_booked');
+        }elseif($vacancies == '0' && $workers == '0'){
+        
+        } 
+ 
+        
+        $results = $builder->get();
+
+        return $results;  
+    }    
 }
